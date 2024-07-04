02fb2812ef946b3a26a7f38a208bf1166c42dd06
shimoda <kuri65536@hotmail.com>
2024-07-04 12:53:51 +0900

    add the build script/ ignored intermediate files.

---
+++ build.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+function build() {
+    nimble build
+}
+
+
+function test() {
+    b=tests/catalog
+    d=$b/en/LC_MESSAGES
+    mkdir -p $d
+    f=test;    msgfmt -o $d/$f.mo $b/$f.po
+    f=another; msgfmt -o $d/$f.mo $b/$f.po
+    testament pattern 'tests/*.nim'
+    testament html
+    mv -f testresults.html html
+}
+
+
+function doc() {
+    nim doc --outdir:html src/i18n.nim
+    cd html; ln -sf i18n.html index.html
+}
+
+
+case "x$1" in
+xdoc)
+    doc
+    ;;
+xtest)
+    test
+    ;;
+*)
+    build
+    ;;
+esac
